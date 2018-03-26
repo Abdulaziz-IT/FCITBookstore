608e9914c2ad36de8c0ee48256db5343f9359888
Abdulaziz-IT <azzoz_4_ever@hotmail.com>
2018-03-26 23:32:40 +0300

    Adding Google API
    Added Google API instead of manually adding the books.
    Removed price property & sort.
    Added authors & page numbers.
    Added page numbers sort.
    Corrected engrish variable names.

--- main.js
+++ main.js
@@ -1,33 +1,53 @@
-function Book(title, stars, image, review, pr, link, date) {
-    //Defining the variables
-    this.tit = title;
-    this.star = stars;
-    this.imageLink = image;
-    this.reviews = review;
-    this.pric = pr;
-    this.lin = link;
-    this.dat = date;
-
-    //Creating the grids
-    createGrids(this.tit, this.star, this.imageLink, this.reviews, this.pric, this.lin, this.dat);
+function run(title) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        // When the request is successful, finished, and response is ready        
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(xhttp.responseText);
+            var title = obj.items[0].volumeInfo.title;
+            if (obj.items[0].volumeInfo.hasOwnProperty('subtitle')) {
+                title += ": " + obj.items[0].volumeInfo.subtitle;
+            }
+            var volumeInfo = obj.items[0].volumeInfo;
+            books.push(new Book(title, volumeInfo.authors[0], volumeInfo.averageRating, volumeInfo.imageLinks.thumbnail,
+                volumeInfo.ratingsCount, volumeInfo.pageCount, volumeInfo.infoLink, volumeInfo.publishedDate));
+        }
+    };
+    // Send an asynchronous HTTP GET request to the given end point (url)
+    xhttp.open("GET", "https://www.googleapis.com/books/v1/volumes?q=" + title + "&key=AIzaSyB_WJVwKPveKKtaNre1qRDqfPseGHZ2HmU", true);
+    xhttp.send();
 }
 
-var books = [];
-books.push(new Book("Web Design with HTML, CSS, JavaScript and jQuery Set", 4.7, "https://images-na.ssl-images-amazon.com/images/I/41T53nRtyoL._SX435_BO1,204,203,200_.jpg", 433, 28.38, "https://www.amazon.com/Web-Design-HTML-JavaScript-jQuery/dp/1118907442/", "July 8, 2014"));
-books.push(new Book("A Smarter Way to Learn JavaScript. The new tech-assisted approach that requires half the effort", 4.7, "https://images-na.ssl-images-amazon.com/images/I/512KPmZIG7L._SX348_BO1,204,203,200_.jpg", 1497, 15.78, "https://www.amazon.com/Smarter-JavaScript-tech-assisted-approach-requires/dp/1497408180/", "March 20, 2014"));
-books.push(new Book("Calculus: Early Transcendentals", 3.9, "https://images-na.ssl-images-amazon.com/images/I/51jitgi3EaL._SX423_BO1,204,203,200_.jpg", 101, 178.46, "https://www.amazon.com/Calculus-Early-Transcendentals-James-Stewart/dp/1285741552", "February 4, 2015"));
-books.push(new Book("A Wrinkle in Time Movie Tie-In Edition (A Wrinkle in Time Quintet)", 4.0, "https://images-na.ssl-images-amazon.com/images/I/51dy4WOGbJL._SX338_BO1,204,203,200_.jpg", 2920, 8.99, "https://www.amazon.com/Wrinkle-Time-Movie-Tie-Quintet/dp/1250153271/", "November 7, 2017"));
-books.push(new Book("Harry Potter And The Chamber Of Secrets", 4.7, "https://images-na.ssl-images-amazon.com/images/I/51jNORv6nQL._SX340_BO1,204,203,200_.jpg", 60226, 7.39, "https://www.amazon.com/Harry-Potter-Chamber-Secrets-Rowling/dp/0439064872/", "August 15, 2000"));
-books.push(new Book("A Game of Thrones (A Song of Ice and Fire, Book 1)", 4.7, "https://images-na.ssl-images-amazon.com/images/I/51fjW-2wOTL._SX328_BO1,204,203,200_.jpg", 20205, 11.29, "https://www.amazon.com/Game-Thrones-Song-Fire-Book/dp/0553386794/", "March 22, 2011"));
-books.push(new Book("Elementary Statistics: A Step By Step Approach", 4.0, "https://images-na.ssl-images-amazon.com/images/I/51bS-0oEmSL._SX388_BO1,204,203,200_.jpg", 359, 122.56, "https://www.amazon.com/Elementary-Statistics-Step-Approach/dp/1259755339/", "February 1, 2017"));
-books.push(new Book("Technical Communication Fundamentals", 3.8, "https://images-na.ssl-images-amazon.com/images/I/51Dd6K-yL8L._SX395_BO1,204,203,200_.jpg", 18, 100.00, "https://www.amazon.com/Technical-Communication-Fundamentals-William-Pfeiffer/dp/0132374579", "March 7, 2011"));
-books.push(new Book("Introduction to Java Programming and Data Structures, Comprehensive Version (11th Edition)", 4.3, "https://images-na.ssl-images-amazon.com/images/I/51rFsKGbyML._SX395_BO1,204,203,200_.jpg", 13, 147.12, "https://www.amazon.com/Introduction-Programming-Structures-Comprehensive-Version/dp/0134670949/", "March 11, 2017"));
+function Book(title, author, stars, image, reviews, page, link, date) {
+    //Defining the variables
+    this.title = title;
+    this.author = author;
+    this.stars = stars;
+    this.image = image;
+    this.reviews = reviews;
+    this.page = page;
+    this.link = link;
+    this.date = date;
 
+    //Creating the grids
+    createGrids(this.title, this.author, this.stars, this.image, this.reviews, this.page, this.link, this.date);
+}
+var books = [];
+run("Harry Potter and the Prisoner of Azkaban");
+run("Game of thrones a song of ice");
+run("How to Lie with Statistics");
+run("Types and Programming Languages");
+run("Thinking in Java");
+run("Winter in Madrid");
+run("curse");
+run("12 Rules for Life");
+run("Little Blue Truck");
+run("Learning Python");
 
 var sorts = document.getElementById("sort");
 sorts.onchange = function () {
-    if (this.value == "price") {
-        books.sort(comparePrice);
+    if (this.value == "page") {
+        books.sort(comparePage);
     } else if (this.value == "review") {
         books.sort(compareReviews);
     } else if (this.value == "stars") {
@@ -38,8 +58,8 @@ sorts.onchange = function () {
     updateDOM();
 }
 
-function comparePrice(a, b) {
-    return a.pric - b.pric;
+function comparePage(a, b) {
+    return b.page - a.page;
 }
 
 function compareReviews(a, b) {
@@ -47,12 +67,12 @@ function compareReviews(a, b) {
 }
 
 function compareStars(a, b) {
-    return b.star - a.star;
+    return b.stars - a.stars;
 }
 
 function compareDates(a, b) {
-    var d1 = new Date(a.dat);
-    var d2 = new Date(b.dat);
+    var d1 = new Date(a.date);
+    var d2 = new Date(b.date);
     if (d1 < d2) {
         return 1;
     } else if (d2 > d1) {
@@ -67,45 +87,58 @@ function updateDOM() {
     allBooks.innerHTML = ""; //Cheating :D
     var i;
     for (i = 0; i < books.length; i++) {
-        createGrids(books[i].tit, books[i].star, books[i].imageLink, books[i].reviews, books[i].pric, books[i].lin, books[i].dat);
+        createGrids(books[i].title, books[i].author, books[i].stars, books[i].image, books[i].reviews, books[i].page, books[i].link, books[i].date);
     }
 }
 
-function createGrids(title, stars, image, review, pr, link, date) {
+function createGrids(title, author, stars, image, review, page, link, date) {
     var allBooks = document.getElementById("grid-container");
     var divElem = document.createElement("div");
     allBooks.appendChild(divElem);
+
     var aElem = document.createElement("a");
     aElem.href = link;
     aElem.target = "_blank";
     divElem.appendChild(aElem);
+
     var imgElem = document.createElement("img");
     imgElem.src = image;
     imgElem.width = "218";
     imgElem.height = "218";
     aElem.appendChild(imgElem);
     aElem.appendChild(document.createElement("br"));
-    titleSpan = document.createElement("span");
+
+    var titleSpan = document.createElement("span");
     titleSpan.className = "title";
     titleSpan.appendChild(document.createTextNode(title));
     aElem.appendChild(titleSpan);
     aElem.appendChild(document.createElement("br"));
-    starsSpan = document.createElement("span");
+
+    var authorSpan = document.createElement("span");
+    authorSpan.className = "authors";
+    authorSpan.appendChild(document.createTextNode("Author: " + author + "."));
+    aElem.appendChild(authorSpan);
+    aElem.appendChild(document.createElement("br"));
+
+    var starsSpan = document.createElement("span");
     starsSpan.className = "stars";
     starsSpan.appendChild(document.createTextNode("Stars: " + stars + " / 5.0."));
     aElem.appendChild(starsSpan);
     aElem.appendChild(document.createElement("br"));
-    reviewSpan = document.createElement("span");
+
+    var reviewSpan = document.createElement("span");
     reviewSpan.className = "reviews";
-    reviewSpan.appendChild(document.createTextNode("Reviews: " + review + " Customer reviews"));
+    reviewSpan.appendChild(document.createTextNode("Reviews: " + review + " Customer reviews."));
     aElem.appendChild(reviewSpan);
     aElem.appendChild(document.createElement("br"));
-    priceSpan = document.createElement("span");
-    priceSpan.className = "price";
-    priceSpan.appendChild(document.createTextNode("Price: " + pr + "$"));
-    aElem.appendChild(priceSpan);
+
+    var pageSpan = document.createElement("span");
+    pageSpan.className = "page";
+    pageSpan.appendChild(document.createTextNode("Number of pages: " + page + " pages."));
+    aElem.appendChild(pageSpan);
     aElem.appendChild(document.createElement("br"));
-    publishSpan = document.createElement("span");
+
+    var publishSpan = document.createElement("span");
     publishSpan.className = "published";
     publishSpan.appendChild(document.createTextNode("Date Published: " + date));
     aElem.appendChild(publishSpan);
